ffd197a114e6bc99d6dba77fec9becb7c73f244e
Lakshmi Yadlapati <lakshmiy@us.ibm.com>
2023-03-31 13:05:12 -0500

    Moving to correct PCIe Device interface
    Move PCIe device interface to
    "xyz.openbmc_project.Inventory.Item.PCIeDevice".
    
    Move peciPCIePath to ""/xyz/openbmc_project/inventory".
    
    bmcweb commit:
    https://gerrit.openbmc.org/c/openbmc/bmcweb/+/62064/1
    
    Tested: None
    
    I don't have environment to test, need help testing this commit.
    
    Change-Id: I75e143a5a688e1b35f49c0abead051589657b8d8
    Signed-off-by: Lakshmi Yadlapati <lakshmiy@us.ibm.com>

--- include/peci_pcie.hpp
+++ include/peci_pcie.hpp
@@ -23,9 +23,9 @@
 namespace peci_pcie
 {
 static constexpr char const* peciPCIeObject = "xyz.openbmc_project.PCIe";
-static constexpr char const* peciPCIePath = "/xyz/openbmc_project/PCIe";
+static constexpr char const* peciPCIePath = "/xyz/openbmc_project/inventory";
 static constexpr char const* peciPCIeDeviceInterface =
-    "xyz.openbmc_project.PCIe.Device";
+    "xyz.openbmc_project.Inventory.Item.PCIeDevice";
 
 static constexpr const int maxPCIBuses = 256;
 static constexpr const int maxPCIDevices = 32;
