134ba1b8dc5c0313c13902c724747a775b2ecff6
Jason M. Bills <jason.m.bills@intel.com>
2024-04-01 13:16:41 -0700

    Use the new CPUModel enum names from libpeci
    libpeci was updated with new CPUModel names, so update uses here to
    match.
    
    Change-Id: I5dddd285ae6bd90f9158ca8e8ff6e2b589ee25ce
    Signed-off-by: Jason M. Bills <jason.m.bills@intel.com>

--- src/peci_pcie.cpp
+++ src/peci_pcie.cpp
@@ -27,6 +27,7 @@
 #include <sdbusplus/bus/match.hpp>
 
 #include <iomanip>
+
 #include <iostream>
 #include <set>
 #include <sstream>
@@ -112,7 +113,7 @@ static resCode getCPUBusMap(std::vector<CPUInfo>& cpuInfo)
 
         switch (model)
         {
-            case skx:
+            case skylake:
             {
                 // Get the assigned CPU bus numbers from CPUBUSNO and CPUBUSNO1
                 // (B(0) D8 F2 offsets CCh and D0h)
